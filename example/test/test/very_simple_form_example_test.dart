183de143b2527c6d6790bd14a4d8c837697794a6
Ernesto Alfonso Caballero <erns70b@github.com>
2025-08-17 13:15:47 +0200

    added form render test to simple example

--- example/test/test/very_simple_form_example_test.dart
+++ example/test/test/very_simple_form_example_test.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 import 'package:example/examples/very_simple_form_example.dart';
 
 void main() {
-  testWidgets('VerySimpleFormExample renders schema title & description', (
-    tester,
-  ) async {
-    await tester.pumpWidget(const MaterialApp(home: VerySimpleFormExample()));
+  testWidgets(
+    'VerySimpleFormExample renders title, description, and the form field',
+    (tester) async {
+      await tester.pumpWidget(const MaterialApp(home: VerySimpleFormExample()));
 
-    // AppBar
-    expect(find.text('DJSF Very Simple Example'), findsOneWidget);
+      // App bar title
+      expect(find.text('DJSF Very Simple Example'), findsOneWidget);
 
-    // Contenido DjsfForm (título y descripción del schema)
-    expect(find.text('A registration form'), findsOneWidget);
-    expect(find.text('Register users'), findsOneWidget);
-  });
+      // Schema title & description
+      expect(find.text('A registration form'), findsOneWidget);
+      expect(find.text('Register users'), findsOneWidget);
+
+      // Field from schema.properties.firstName
+      // Since properties.firstName has no "title", the label defaults to the key name: "firstName"
+      expect(
+        find.widgetWithText(ReactiveTextField<String>, 'firstName'),
+        findsOneWidget,
+      );
+    },
+  );
 }
